bebc3e2fd4b3744e11189220cb1f9cd0563ff37b
EthanSchlosser <24schle@live.elida.k12.oh.us>
2022-03-30 11:09:37 -0400

    golf club physics updated

---
+++ Assets/02 - Mini Golf/Scripts/PutterForce.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutterForce : MonoBehaviour
+{
+    public float radius = 5.0F;
+    public float power = 10.0F;
+    public AudioSource src;
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collide");
+        if (col.gameObject.tag == "Ball")
+        {
+            Debug.Log("Ball");
+            Vector3 explosionPos = transform.position;
+            Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+
+
+            if (rb != null)
+            {
+                Debug.Log("Boom");
+                src.Play();
+                rb.AddExplosionForce(power, explosionPos, radius, 1.0F);
+            }
+        }
+    }
+}
---
+++ Assets/02 - Mini Golf/Scripts/PutterForce2Angle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutterForce2Angle : MonoBehaviour
+{
+    public float radius = 5.0F;
+    public float power = 10.0F;
+    public AudioSource src;
+    public float angle = 45;
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collide");
+        if (col.gameObject.tag == "Ball")
+        {
+            Debug.Log("Ball");
+            Vector3 explosionPos = transform.position;
+            Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+
+            //Get angle of club head
+            Vector3 dir = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
+
+            if (rb != null)
+            {
+                Debug.Log("Boom");
+                src.Play();
+                //rb.AddExplosionForce(power, explosionPos, radius, 1.0F);
+                rb.AddForce(dir * power, ForceMode.Impulse);
+            }
+        }
+    }
+}
